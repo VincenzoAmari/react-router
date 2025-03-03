a6779236ffab0709786c9a96887487b268f0452a
VincenzoAmari <vincenzo.am99@gmail.com>
2025-03-03 16:58:36 +0100

    collegamento con api crud DB

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,5 +1,14 @@
-const Posts = () => {
-    return <h2>Lista dei post</h2>;
-  };
+import { useEffect, useState } from "react";
+
+function Posts() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/api/posts")
+      .then((res) => res.json())
+      .then((data) => setPosts(data))
+      .catch((error) => console.error("Errore nel caricamento dei post:", error));
+  }, []);
+}
   
   export default Posts;
