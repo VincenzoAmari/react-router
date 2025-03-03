f03fd180bde28d50fac6b877ea1e275b9a245130
VincenzoAmari <vincenzo.am99@gmail.com>
2025-03-03 15:17:18 +0100

    corretto app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,19 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, NavLink } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Posts from "./pages/Posts";
+import Layout from "./components/Layout";
 
 function App() {
   return (
     <Router>
-      <nav>
-        <NavLink to="/" className={({ isActive }) => (isActive ? "active" : "")}>Home</NavLink>
-        <NavLink to="/about" className={({ isActive }) => (isActive ? "active" : "")}>Chi siamo</NavLink>
-        <NavLink to="/posts" className={({ isActive }) => (isActive ? "active" : "")}>Post</NavLink>
-      </nav>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/posts" element={<Posts />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="posts" element={<Posts />} />
+        </Route>
       </Routes>
     </Router>
   );
