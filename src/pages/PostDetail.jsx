9362b7e55e2afe3881e7ebf86c962e7be7e922c3
VincenzoAmari <vincenzo.am99@gmail.com>
2025-03-03 17:49:46 +0100

    inizio a prendere il singolo post

---
+++ src/pages/PostDetail.jsx
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from "react";
+import { useParams, Link } from "react-router-dom";
+
+function PostDetail() {
+  const { id } = useParams(); // ID  URL
+  const [post, setPost] = useState(null);
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/api/posts")
+      .then((response) => response.json())
+      .then((data) => {
+        setPosts(data);
+        const foundPost = data.find((p) => p.id === parseInt(id));
+        setPost(foundPost);
+      })
+      .catch((error) => console.error("Errore nel caricamento:", error));
+  }, [id]);
+
+}
