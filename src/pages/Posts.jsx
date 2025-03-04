2a512a3d208ced298694319d59306d73722e76da
VincenzoAmari <vincenzo.am99@gmail.com>
2025-03-04 10:04:30 +0100

    piccole modifiche

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -16,17 +16,24 @@ function Posts() {
   return (
     <div className="container">
       <h1>Lista dei Post</h1>
+
       <div className="posts-container">
-        {posts.map((post) => (
-          <div key={post.id} className="post-card" onClick={() => navigate(`/posts/${post.id}`)}>
+
+        {/* MAP */}
+
+        {posts.map((post) => (   
+          <div key={post.id} className="post-card" onClick={() => navigate(`/posts/${post.id}`)}> 
             <h3>{post.titolo || "Senza titolo"}</h3>
+
             <img 
               src={`http://localhost:3000${post.immagine}`} 
               alt={post.titolo} 
               onError={(e) => { e.target.src = "https://via.placeholder.com/300"; }} 
             />
+
             <p>{post.contenuto || "Nessun contenuto disponibile"}</p>
             <p className="tags"><strong>Tags:</strong> {post.tags ? post.tags.join(", ") : "Nessun tag"}</p>
+
             <button className="details-link" onClick={() => navigate(`/posts/${post.id}`)}>Leggi di più</button>
           </div>
         ))}
