f05b1911ca235c9dc03de4a1895269462012eed9
VincenzoAmari <vincenzo.am99@gmail.com>
2025-03-05 11:27:01 +0100

    update layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,27 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, NavLink } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  NavLink,
+} from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Posts from "./pages/Posts";
-import PostDetail from "./pages/PostDetail"; 
+import PostDetail from "./pages/PostDetail";
+import Layout from "./components/Layout";
 
 function App() {
   return (
     <Router>
-      <nav>
-        <NavLink to="/" className={({ isActive }) => (isActive ? "active" : "")}>Home</NavLink>
-        <NavLink to="/about" className={({ isActive }) => (isActive ? "active" : "")}>Chi siamo</NavLink>
-        <NavLink to="/posts" className={({ isActive }) => (isActive ? "active" : "")}>Post</NavLink>
-      </nav>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/posts" element={<Posts />} />
-        <Route path="/posts/:id" element={<PostDetail />} /> {/* Aggiungiamo la pagina di dettaglio */}
+        <Route path="/" element={<Layout />}>
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/posts" element={<Posts />} />
+          <Route path="/posts/:id" element={<PostDetail />} />{" "}
+          {/* Aggiungiamo la pagina di dettaglio */}
+        </Route>
       </Routes>
     </Router>
   );
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -4,9 +4,24 @@ function Layout() {
   return (
     <>
       <nav>
-        <NavLink to="/" className={({ isActive }) => (isActive ? "active" : "")}>Home</NavLink>
-        <NavLink to="/about" className={({ isActive }) => (isActive ? "active" : "")}>Chi siamo</NavLink>
-        <NavLink to="/posts" className={({ isActive }) => (isActive ? "active" : "")}>Post</NavLink>
+        <NavLink
+          to="/home"
+          className={({ isActive }) => (isActive ? "active" : "")}
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to="/about"
+          className={({ isActive }) => (isActive ? "active" : "")}
+        >
+          Chi siamo
+        </NavLink>
+        <NavLink
+          to="/posts"
+          className={({ isActive }) => (isActive ? "active" : "")}
+        >
+          Post
+        </NavLink>
       </nav>
       <main>
         <Outlet />
